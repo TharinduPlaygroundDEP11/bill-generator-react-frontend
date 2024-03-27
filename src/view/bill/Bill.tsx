355024275dddd6bd38738334cf52b191ba4f11c2
Tharindu Nuwan Madhushanka <tharindunm94@gmail.com>
2024-03-27 12:38:49 +0530

    Implement downloading bill with customized pdf

--- src/view/bill/Bill.tsx
+++ src/view/bill/Bill.tsx
@@ -1,9 +1,9 @@
 import { useNavigate } from "react-router-dom";
 import { useBillStore } from "../../Store";
 import "./Bill.css";
-import html2canvas from "html2canvas";
 import jsPDF from "jspdf";
 import { ToastContainer, toast } from "react-toastify";
+import 'jspdf-autotable'
 
 function Bill() {
   const navigate = useNavigate();
@@ -21,53 +21,89 @@ function Bill() {
     totalAmount,
   } = useBillStore();
 
-  const toastError = (message:string)=>{
-    toast.error(`${message}`, {
+  const toastSuccess = (message: string) => {
+    toast.success(`${message}`, {
       position: "top-center",
-      theme: "colored"
+      theme: "colored",
     });
   };
 
-  const toastSuccess = (message:string)=>{
-    toast.success(`${message}`, {
+  const handlePayClick = () => {
+    toast.warning("The payment options not implemented yet!", {
       position: "top-center",
-      theme: "colored"
+      theme: "colored",
     });
+  };
+
+  const formatDate = (date: Date) => {
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, "0");
+    const day = String(date.getDate()).padStart(2, "0");
+    return `${year}-${month}-${day}`;
   };
 
   const handleDownloadBill = () => {
-    const content = document.getElementById("content");
-    const elementsToHide = content!.querySelectorAll(".btn-close, .bill-image") as NodeListOf<HTMLElement>;
-    elementsToHide.forEach((element) => {
-      element.style.display = "none";
-    });
+    const today = new Date();
+    const dueDate = new Date(
+      today.getFullYear(),
+      today.getMonth() + 2,
+      today.getDate()
+    );
+    const invoiceNumber = "INV-2024001";
 
-    html2canvas(content!, { scale: 4 })
-      .then((canvas) => {
-        const imgData = canvas.toDataURL("image/png");
-        const pdf = new jsPDF("l", "pt", "a5");
-        const pdfWidth = pdf.internal.pageSize.getWidth();
-        const imgWidth = pdfWidth - 20;
-        const imgHeight = (canvas.height * imgWidth) / canvas.width;
+    const pdf = new jsPDF("p", "pt", "a5");
 
-        pdf.addImage(imgData, "PNG", 10, 10, imgWidth, imgHeight);
+    const pageWidth = pdf.internal.pageSize.getWidth();
 
-        elementsToHide.forEach((element) => {
-          element.style.display = "block";
-        });
+    pdf.setFont("arial",  'bold');
+    pdf.setFontSize(18);
+    pdf.text("Electricity Bill", 150, 30);
 
-        pdf.save("downloaded-bill.pdf");
-        toastSuccess("Your bill is downloaded");
-      })
-      .catch((err) => {
-        console.log(err);
-        toastError("Failed to download the bill, try again!");
-      });
+    pdf.setFont("arial", "normal");
+    pdf.setFontSize(12);
+
+    pdf.text(`Invoice Number : ${invoiceNumber}`, 30, 55);
+    pdf.text(`Invoice Date : ${formatDate(today)}`, 30, 75); 
+    pdf.text(`Due Date : ${formatDate(dueDate)}`, 30, 95);
+    pdf.text(`Account Number : ${accountNumber}`, 30, 115);
+    pdf.text(`Customer Name : ${accountNumber}`, 30, 135);
+    
+    pdf.text("Last Reading Date : ", 60, 180);
+    pdf.text(`${lastDate}`, (pageWidth - 60 - (pdf.getStringUnitWidth(`${lastDate}`) * 12 / pdf.internal.scaleFactor)), 180);
+    
+    pdf.text('Last Reading Value : ', 60, 200);
+    pdf.text(`${lastValue}`, (pageWidth - 60 - (pdf.getStringUnitWidth(`${lastValue}`) * 12 / pdf.internal.scaleFactor)), 200);
+    
+    pdf.text('Previous Reading Date : ', 60, 220);
+    pdf.text(`${previousDate}`, (pageWidth - 60 - (pdf.getStringUnitWidth(`${previousDate}`) * 12 / pdf.internal.scaleFactor)), 220);
+
+    pdf.text('Previous Reading Value : ', 60, 240);
+    pdf.text(`${previousValue}`, (pageWidth - 60 - (pdf.getStringUnitWidth(`${previousValue}`) * 12 / pdf.internal.scaleFactor)), 240);
+    
+    pdf.text('First Range Amount : ', 60, 260);
+    pdf.text(`Rs.${firstRangeAmount}`, (pageWidth - 60 - (pdf.getStringUnitWidth(`Rs.${firstRangeAmount}`) * 12 / pdf.internal.scaleFactor)), 260);
+
+    pdf.text('Second Range Amount : ', 60, 280);
+    pdf.text(`Rs.${secondRangeAmount}`, (pageWidth - 60 - (pdf.getStringUnitWidth(`Rs.${secondRangeAmount}`) * 12 / pdf.internal.scaleFactor)), 280);
+    
+    pdf.text('Third Range Amount : ', 60, 300);
+    pdf.text(`Rs.${thirdRangeAmount}`, (pageWidth - 60 - (pdf.getStringUnitWidth(`Rs.${thirdRangeAmount}`) * 12 / pdf.internal.scaleFactor)), 300);
+
+    pdf.text('Total Units : ', 60, 320);
+    pdf.text(`${totalUnits}`, (pageWidth - 60 - (pdf.getStringUnitWidth(`${totalUnits}`) * 12 / pdf.internal.scaleFactor)), 320);
+
+
+    pdf.setFont("arial",  'bold');
+    pdf.setFontSize(14);
+    pdf.text('Total Amount : ', 60, 360);
+    pdf.text(`Rs.${totalAmount}`, (pageWidth - 60 - (pdf.getStringUnitWidth(`Rs.${totalAmount}`) * 14 / pdf.internal.scaleFactor)), 360);
+
+    pdf.save("ceb-bill.pdf");
   };
 
   return (
     <>
-    <ToastContainer closeOnClick autoClose={1000}/>
+      <ToastContainer closeOnClick autoClose={1000} />
       <div className="bill-bg vw-100 vh-100 d-flex justify-content-center align-items-center red-hat">
         <div className="bill-container d-flex flex-column shadow-lg p-3 mb-5 bg-body-tertiary rounded">
           <div id="content">
@@ -132,7 +168,10 @@ function Bill() {
             </div>
           </div>
           <div className="bill-footer border-top pt-2 mt-1 d-flex justify-content-end">
-            <button className="bill-button d-flex justify-content-center align-items-center gap-1">
+            <button
+              className="bill-button d-flex justify-content-center align-items-center gap-1"
+              onClick={handlePayClick}
+            >
               <i className="bi bi-credit-card"></i> Pay
             </button>
             <button
